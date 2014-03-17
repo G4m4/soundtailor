54a2838071120188ddfa0a9764bd0488ca5fb7ff
Gama <misept.dieseneuf@gmail.com>
2014-03-17 19:52:50 +0100

    GENERATORS: Add base generators stuff
    - Common generators interface
    - Common helper functions & classes
    
    Signed-off-by: Gama <misept.dieseneuf@gmail.com>

---
+++ soundtailor/src/generators/generators_common.cc
@@ -0,0 +1,79 @@
+/// @file generators_common.cc
+/// @brief SoundTailor common stuff for all generators - implementation
+/// @author gm
+/// @copyright gm 2014
+///
+/// This file is part of SoundTailor
+///
+/// SoundTailor is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU General Public License as published by
+/// the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+///
+/// SoundTailor is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+///
+/// You should have received a copy of the GNU General Public License
+/// along with SoundTailor.  If not, see <http://www.gnu.org/licenses/>.
+
+#include "soundtailor/src/generators/generators_common.h"
+#include "soundtailor/src/maths.h"
+
+namespace soundtailor {
+namespace generators {
+
+// PhaseAccumulator
+
+PhaseAccumulator::PhaseAccumulator(const float phase)
+    : Generator_Base(phase),
+      phase_(Fill(phase)),
+      increment_(Fill(0.0f)) {
+  ASSERT(phase <= 1.0f);
+  ASSERT(phase >= -1.0f);
+}
+
+Sample PhaseAccumulator::operator()(void) {
+  const Sample out(phase_);
+  phase_ = IncrementAndWrap(phase_, increment_);
+  return out;
+}
+
+void PhaseAccumulator::SetPhase(const float phase) {
+  ASSERT(phase <= 1.0f);
+  ASSERT(phase >= -1.0f);
+  // If we are not sure, we can use the following:
+  // phase_ = Wrap(phase);
+  phase_ = FillIncremental(phase, GetByIndex<0>(increment_));
+}
+
+void PhaseAccumulator::SetFrequency(const float frequency) {
+  ASSERT(frequency >= 0.0f);
+  ASSERT(frequency <= 0.5f);
+
+  const float base_increment(2.0f * frequency);
+  increment_ = FillOnLength(base_increment);
+  phase_ = FillIncremental(Phase(), base_increment);
+}
+
+float PhaseAccumulator::Phase(void) const {
+  return GetByIndex<0>(phase_);
+}
+
+Differentiator::Differentiator(const float last)
+    : last_(last) {
+  // Nothing to do here
+}
+
+Sample Differentiator::operator()(SampleRead sample) {
+  const float before_diff(GetByIndex<3>(sample));
+  const Sample prev(RotateOnRight(sample,
+                                  last_));
+  const Sample after_diff(Sub(sample, prev));
+  last_ = before_diff;
+  return after_diff;
+}
+
+}  // namespace generators
+}  // namespace soundtailor
---
+++ soundtailor/src/generators/generators_common.h
@@ -0,0 +1,61 @@
+/// @file generators_common.h
+/// @brief SoundTailor common stuff for all generators - declarations
+/// @author gm
+/// @copyright gm 2014
+///
+/// This file is part of SoundTailor
+///
+/// SoundTailor is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU General Public License as published by
+/// the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+///
+/// SoundTailor is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+///
+/// You should have received a copy of the GNU General Public License
+/// along with SoundTailor.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef SOUNDTAILOR_SRC_GENERATORS_GENERATORS_COMMON_H_
+#define SOUNDTAILOR_SRC_GENERATORS_GENERATORS_COMMON_H_
+
+#include "soundtailor/src/common.h"
+#include "soundtailor/src/generators/generator_base.h"
+
+namespace soundtailor {
+namespace generators {
+
+/// @brief Basic sawtooth signal generator
+/// Generates a lot of aliasing, not to be used straight to audio
+class PhaseAccumulator : public Generator_Base {
+ public:
+  explicit PhaseAccumulator(const float phase = 0.0f);
+  virtual Sample operator()(void);
+  virtual void SetPhase(const float phase);
+  virtual void SetFrequency(const float frequency);
+  virtual float Phase(void) const;
+
+ protected:
+  Sample phase_;  ///< Instantaneous phase of the generator
+
+ private:
+  Sample increment_;  ///< Increment to be accumulated at each iteration
+};
+
+/// @brief Basic differentiator
+/// implementing a simple 1st-order differentiator, unitary gain
+class Differentiator {
+ public:
+  explicit Differentiator(const float last = 0.0f);
+  Sample operator()(SampleRead sample);
+
+ private:
+  float last_;  ///< Last synthesized sample value
+};
+
+}  // namespace generators
+}  // namespace soundtailor
+
+#endif  // SOUNDTAILOR_SRC_GENERATORS_GENERATORS_COMMON_H_
