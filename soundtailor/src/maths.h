4a7c51bdd9f30fd653003875eeab6e58cc2b2484
Gama <misept.dieseneuf@gmail.com>
2014-03-17 19:51:00 +0100

    COMMON: Add a common math headers
    This header (no implementation file) supply base functions to be used
    whenever Sample are computed, in order to benefit from vectorize
    instructions.
    
    Signed-off-by: Gama <misept.dieseneuf@gmail.com>

---
+++ soundtailor/src/maths.h
@@ -0,0 +1,360 @@
+/// @file maths.h
+/// @brief SoundTailor common maths header
+/// @author gm
+/// @copyright gm 2014
+///
+/// This file is part of SoundTailor
+///
+/// SoundTailor is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU General Public License as published by
+/// the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+///
+/// SoundTailor is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+///
+/// You should have received a copy of the GNU General Public License
+/// along with SoundTailor.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef SOUNDTAILOR_SRC_MATHS_H_
+#define SOUNDTAILOR_SRC_MATHS_H_
+
+#include <cmath>
+// std::min, std::max
+#include <algorithm>
+
+// Has to be included BEFORE SSE intrinsics related headers,
+// in order to know if these headers actually have to be included
+#include "soundtailor/src/configuration.h"
+
+#if (_USE_SSE)
+extern "C" {
+#include <emmintrin.h>
+#include <mmintrin.h>
+}
+#endif  // (_USE_SSE)
+
+#include "soundtailor/src/common.h"
+
+namespace soundtailor {
+
+/// @brief Fill a whole Sample with the given value
+///
+/// @param[in]  value   Value to be copied through the whole Sample
+static inline Sample Fill(const float value) {
+#if (_USE_SSE)
+  return _mm_set1_ps(value);
+#else
+  return value;
+#endif  // (_USE_SSE)
+}
+
+/// @brief Fill a whole Sample with the given float array
+///
+/// @param[in]  value   Pointer to the float array to be used:
+///                     must be SampleSizeBytes long
+static inline Sample Fill(const float* value) {
+#if (_USE_SSE)
+  return _mm_loadu_ps(value);
+#else
+  return *value;
+#endif  // (_USE_SSE)
+}
+
+/// @brief Fill a whole Sample with all given scalars,
+/// beware of the order!
+///
+/// @param[in]  a   Last value
+/// @param[in]  b   Second to last value
+/// @param[in]  c   Second value
+/// @param[in]  d   First value
+static inline Sample Fill(const float a,
+                          const float b,
+                          const float c,
+                          const float d) {
+#if (_USE_SSE)
+  return _mm_set_ps(a, b, c, d);
+#else
+  IGNORE(a);
+  IGNORE(b);
+  IGNORE(c);
+  return d;
+#endif  // (_USE_SSE)
+}
+
+/// @brief Fill a whole Sample with incremental values as follows:
+///
+/// First value:  base
+/// ...
+/// Last value:  base + SampleSize * increment
+///
+/// @param[in]  base    Base value to fill the first element of the Sample with
+/// @param[in]  increment    Value to add at each Sample element
+static inline Sample FillIncremental(const float base,
+                                     const float increment) {
+  return Fill(base + increment * 3.0f,
+              base + increment * 2.0f,
+              base + increment,
+              base);
+}
+
+/// @brief Fill a whole Sample based on its length
+///
+/// The actual value is SampleSize * given value
+///
+/// @param[in]  base    Base value to be filled with
+static inline Sample FillOnLength(const float base) {
+  return Fill(base * soundtailor::SampleSize);
+}
+
+/// @brief Helper union for vectorized type to scalar array conversion
+typedef union {
+  Sample sample_v;  ///< Vectorized type
+  float sample[soundtailor::SampleSize];  ///< Array of scalars
+} ConverterFloatScalarVector;
+
+/// @brief Extract one element from a Sample (compile-time version)
+///
+/// @param[in]  input   Sample to be read
+template<unsigned i>
+float GetByIndex(SampleRead input) {
+#if (_USE_SSE)
+  ConverterFloatScalarVector converter;
+  converter.sample_v = input;
+  return converter.sample[i];
+#else
+  return input;
+#endif  // (_USE_SSE)
+}
+
+/// @brief Extract one element from a Sample (runtime version, in loops)
+///
+/// @param[in]  input   Sample to be read
+/// @param[in]  i   Index of the element to retrieve
+static inline float GetByIndex(SampleRead input, const unsigned i) {
+  ConverterFloatScalarVector converter;
+  converter.sample_v = input;
+  return converter.sample[i];
+}
+
+/// @brief Add "left" to "right"
+static inline Sample Add(SampleRead left, SampleRead right) {
+#if (_USE_SSE)
+  return _mm_add_ps(left, right);
+#else
+  return left + right;
+#endif  // (_USE_SSE)
+}
+
+/// @brief Sum all elements of a Sample
+static inline float AddHorizontal(SampleRead input) {
+#if (_USE_SSE)
+  const Sample first_add(Add(input, _mm_movehl_ps(input, input)));
+  const Sample shuffled(_mm_shuffle_ps(first_add, first_add,
+                                       _MM_SHUFFLE(0, 1, 0, 1)));
+  return GetByIndex<0>(Add(shuffled, first_add));
+#else
+  return input;
+#endif  // (_USE_SSE)
+}
+
+/// @brief Substracte "right" from "left"
+static inline Sample Sub(SampleRead left, SampleRead right) {
+#if (_USE_SSE)
+  return _mm_sub_ps(left, right);
+#else
+  return left - right;
+#endif  // (_USE_SSE)
+}
+
+/// @brief Add "left" to "right"
+///
+/// @param[in]  left   First Sample to be added
+/// @param[in]  right   Second Sample to be added
+static inline Sample Mul(SampleRead left, SampleRead right) {
+#if (_USE_SSE)
+  return _mm_mul_ps(left, right);
+#else
+  return left * right;
+#endif  // (_USE_SSE)
+}
+
+/// @brief Shift to left all elements of the input by 1,
+/// and shift in the given value
+///
+/// E.g. given (x_{n}, x_{n + 1}, x_{n + 2}, x_{n + 3})
+/// return (value, x_{n}, x_{n + 1}, x_{n + 2})
+///
+/// @param[in]  input   Sample to be shifted
+/// @param[in]  value   value to be shifted in
+static inline Sample RotateOnRight(SampleRead input,
+                                   const float value) {
+#if (_USE_SSE)
+  const Sample rotated(_mm_castsi128_ps(
+                       _mm_slli_si128(_mm_castps_si128(input), 4)));
+  return Add(_mm_set_ss(value), rotated);
+#else
+  IGNORE(input);
+  return value;
+#endif  // (_USE_SSE)
+}
+
+/// @brief Return the sign of each element of the Sample
+///
+/// Sgn(0.0) return 0.0
+static inline Sample Sgn(SampleRead input) {
+#if (_USE_SSE)
+  const Sample kZero(_mm_setzero_ps());
+  const Sample kOne(Fill(1.0f));
+  const Sample kMinus(Fill(-1.0f));
+  const Sample kPlusMask(_mm_and_ps(_mm_cmpgt_ps(input, kZero), kOne));
+  const Sample kMinusMask(_mm_and_ps(_mm_cmplt_ps(input, kZero), kMinus));
+  return Add(kPlusMask, kMinusMask);
+#else
+  return Sub((0.0f < input), (input < 0.0f));
+#endif  // (_USE_SSE)
+}
+
+/// @brief Return the sign of each element of the Sample, no zero version
+///
+/// Sgn(0.0) return 1.0f
+static inline Sample SgnNoZero(SampleRead value) {
+#if (_USE_SSE)
+  const Sample kZero(_mm_setzero_ps());
+  const Sample kOne(Fill(1.0f));
+  const Sample kMinus(Fill(-1.0f));
+  const Sample kPlusMask(_mm_and_ps(_mm_cmpge_ps(value, kZero), kOne));
+  const Sample kMinusMask(_mm_and_ps(_mm_cmplt_ps(value, kZero), kMinus));
+  return Add(kPlusMask, kMinusMask);
+#else
+  return Sub((0.0f <= value), (value < 0.0f));
+#endif  // (_USE_SSE)
+}
+
+/// @brief Store the given Sample into memory
+///
+/// @param[in]  buffer   Memory to be filled with the input
+/// @param[in]  input   Sampel to be stored
+static inline void Store(float* const buffer, SampleRead input) {
+#if (_USE_SSE)
+  _mm_storeu_ps(buffer, input);
+#else
+  *buffer = input;
+#endif  // (_USE_SSE)
+}
+
+#if (_USE_SSE)
+/// @brief Get each right half of the two given vectors
+///
+/// Given left = (x0, x1, x2, x3) and right = (y0, y1, y2, y3)
+/// it will return (x2, x3, y2, y3)
+static inline Sample TakeEachRightHalf(SampleRead left,
+                                       SampleRead right) {
+  return _mm_shuffle_ps(right, left, _MM_SHUFFLE(3, 2, 3, 2));
+}
+#endif  // (_USE_SSE)
+
+/// @brief Revert the given vector values order
+///
+/// Given value = (x0, x1, x2, x3)
+/// it will return (x3, x2, x1, x0)
+static inline Sample Revert(SampleRead value) {
+#if (_USE_SSE)
+  return _mm_shuffle_ps(value, value, _MM_SHUFFLE(0, 1, 2, 3));
+#else
+  return value;
+#endif  // (_USE_SSE)
+}
+
+/// @brief Return each min element of both inputs
+static inline Sample Min(SampleRead left, SampleRead right) {
+#if (_USE_SSE)
+  return _mm_min_ps(left, right);
+#else
+  return std::min(left, right);
+#endif  // (_USE_SSE)
+}
+
+/// @brief Return each max element of both inputs
+static inline Sample Max(SampleRead left, SampleRead right) {
+#if (_USE_SSE)
+  return _mm_max_ps(left, right);
+#else
+  return std::max(left, right);
+#endif  // (_USE_SSE)
+}
+
+/// @brief Round each Sample element to the nearest integer
+static inline Sample Round(SampleRead input) {
+#if (_USE_SSE)
+  const Sample kZero(_mm_setzero_ps());
+  const Sample kPlus(Fill(0.5f));
+  const Sample kMinus(Fill(-0.5f));
+  const Sample kPlusMask(_mm_and_ps(_mm_cmpge_ps(input, kZero), kPlus));
+  const Sample kMinusMask(_mm_and_ps(_mm_cmplt_ps(input, kZero), kMinus));
+  const Sample kAddMask(Add(kPlusMask, kMinusMask));
+  return Add(kAddMask, input);
+#else
+  return (input > 0.0f) ? (input + 0.5f) : (input - 0.5f);
+#endif  // (_USE_SSE)
+}
+
+/// @brief Helper function: increment the input and wraps it into [-1.0 ; 1.0[
+///
+/// @param[in]  input         Input to be wrapped - supposed not to be < 1.0
+/// @param[in]  increment     Increment to add to the input
+///@return the incremented output in [-1.0 ; 1.0[
+static inline Sample IncrementAndWrap(SampleRead input, SampleRead increment) {
+  // TODO(gm): check if a common code path can be found
+#if (_USE_SSE)
+  const Sample output(Add(input, increment));
+  const Sample constant(Fill(-2.0f));
+  const Sample threshold(Fill(1.0f));
+  const Sample addition_mask(_mm_cmpgt_ps(output, threshold));
+  const Sample add(_mm_and_ps(addition_mask, constant));
+  return Add(output, add);
+#else
+  Sample output(Add(input, increment));
+  if (output > 1.0f) {
+    const float constant(-2.0f);
+    output = Add(output, constant);
+  }
+  return output;
+#endif  // (_USE_SSE)
+}
+
+
+// From this point, intrinsics are no longer used,
+// these are only composition of above functions
+
+
+/// @brief Helper function: limit input into [min ; max]
+static inline Sample Clamp(SampleRead input,
+                           const SampleRead min,
+                           const SampleRead max) {
+  return Min(Max(input, min), max);
+}
+
+
+/// @brief Multiply a Sample by a scalar constant
+///
+/// @param[in]  constant   Scalar constant to multiply the Sample by
+/// @param[in]  input   Sample to be multiplied
+static inline Sample MulConst(const float constant, SampleRead input) {
+  return Mul(Fill(constant), input);
+}
+
+/// @brief Return the absolute value of each element of the Sample
+static inline Sample Abs(SampleRead input) {
+#if (_USE_SSE)
+  return Max(Sub(Fill(0.0f), input), input);
+#else
+  return std::fabs(input);
+#endif  // (_USE_SSE)
+}
+
+}  // namespace soundtailor
+
+#endif  // SOUNDTAILOR_SRC_MATHS_H_
