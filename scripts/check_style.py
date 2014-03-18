fc37dcf0b63ccf4a58aef1f4d5d54671ab35410d
Gama <misept.dieseneuf@gmail.com>
2014-03-18 17:01:36 +0100

    SCRIPTS: Add base common scripts
    - Add Pydev project files
    - Add Cpplint-related script in order to check code style
    - Add common utilities to all DSP scripts
    - Add filters, generators and modulators common definition files
    
    Signed-off-by: Gama <misept.dieseneuf@gmail.com>

---
+++ scripts/check_style.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+'''
+@file check_style.py
+@brief Various utility functions/classes for checking C++ code style
+@author gm
+@copyright gm 2014
+
+This file is part of SoundTailor
+
+SoundTailor is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+SoundTailor is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with SoundTailor.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+'''
+Note that all of this mimics C++ code for testing/prototyping purpose.
+Hence it may not really seems "pythonic" and not intended to be in any way.
+'''
+
+import os
+
+import cpplint
+
+kIgnoredFilenames = ()
+
+def CheckRecursivelyCppFiles(root_path, file_exts, arguments):
+    '''
+    Recursively browse through all subfiles in "root_path" and applies cpplint
+    to each one of them suffixed with one of the "file_exts" extension
+    cpplint gets called with "arguments"
+    '''
+    absolute_root = os.path.abspath(root_path)
+    concatenated_filenames = ""
+    for sub_root,filenames in [(sub_root,filenames) for sub_root,_,filenames in os.walk(absolute_root)]:
+        for filename in [filename for filename in filenames if (filename not in kIgnoredFilenames)]:
+            if os.path.splitext(filename)[1] in file_exts:
+                concatenated_filenames += os.path.join(sub_root, filename) + ' '
+    arguments.append(concatenated_filenames)
+
+    filenames = cpplint.ParseArguments(arguments)
+    cpplint._cpplint_state.ResetErrorCounts()
+    for filename in filenames[0].split(' '):
+        cpplint.ProcessFile(filename, cpplint._cpplint_state.verbose_level)
+    cpplint._cpplint_state.PrintErrorCounts()
+
+if __name__ == "__main__":
+    root_path = os.path.join(os.path.join(os.path.realpath(__file__),
+                                          os.pardir,
+                                          os.pardir),
+                             "SoundTailor")
+    file_exts = (".cc", ".cpp", ".h")
+    arguments = ["--filter=-whitespace/comments,-build/header_guard",
+                 "--counting=detailed",
+                 "--output=vs7"]
+    CheckRecursivelyCppFiles(root_path, file_exts, arguments)
---
+++ scripts/extract_test_data.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+'''
+@file extract_test_data.py
+@brief Utilities for retrieving generated test data 
+@author gm
+@copyright gm 2014
+
+This file is part of SoundTailor
+
+SoundTailor is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+SoundTailor is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with SoundTailor.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+import numpy
+import os
+
+def ExtractSignal(filename):
+    '''
+    Open a file and extract a signal from it
+
+    the file is supposed to be organized as follows:
+    sample,\n
+    sample,\n
+    ...
+    sample,\n
+
+    Return a numpy signal
+    '''
+    if not os.path.exists(filename):
+        raise Exception("No such file: "  + str(filename))
+    return numpy.fromfile(filename, sep='\n')
+
+if __name__ == "__main__":
+    import pylab
+    import utilities
+    import os.path
+    import scipy.io.wavfile
+
+    files = ('../build/soundtailor/tests/in.dat',
+             '../build/soundtailor/tests/out.dat',
+#            '../build/soundtailor/tests/expected.dat',
+             )
+
+    view_beginning = 33000
+    view_length = 256
+
+    signals = []
+    for filename in files:
+        signal_label = os.path.basename(filename)
+        print ("File '" + signal_label + "': " + str(filename))
+        signal = ExtractSignal(filename)
+        metas = utilities.GetMetadata(signal)
+        print(utilities.PrintMetadata(metas))
+        pylab.plot(signal[view_beginning:view_beginning + view_length:1], label=signal_label)
+        signals.append(signal)
+        utilities.WriteWav(signal, filename, 96000.0)
+
+    pylab.legend()
+
+#     pylab.plot((utilities.Diff(signals[0]))[view_beginning:view_beginning + view_length:1])
+    print(numpy.max(utilities.Diff(signals[0])))
+    print(utilities.ZeroCrossings(utilities.Diff(signals[0])))
+#     pylab.plot((signals[1] - signals[0])[view_beginning:view_beginning + view_length:1])
+    pylab.show()
