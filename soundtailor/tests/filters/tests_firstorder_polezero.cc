a2212999dc14bd61a106f6762b86f5d4053f9463
Gama <misept.dieseneuf@gmail.com>
2014-04-03 12:00:22 +0200

    FILTERS: Add basic tests for 1st order Pole-Zero lowpass
    These pass in both scalar and vectorized versions.
    Fixes #17.
    
    Signed-off-by: Gama <misept.dieseneuf@gmail.com>

---
+++ soundtailor/tests/filters/tests_firstorder_polezero.cc
@@ -0,0 +1,116 @@
+/// @file tests_firstorder_polezero.cc
+/// @brief SoundTailor 1st order Pole-Zero filters tests
+/// @author gm
+/// @copyright gm 2014
+///
+/// This file is part of SoundTailor
+///
+/// SoundTailor is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU General Public License as published by
+/// the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+///
+/// SoundTailor is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+///
+/// You should have received a copy of the GNU General Public License
+/// along with SoundTailor.  If not, see <http://www.gnu.org/licenses/>.
+
+#include "soundtailor/tests/tests.h"
+
+#include "soundtailor/src/filters/firstorder_polezero.h"
+
+// Using declarations for tested filter
+using soundtailor::filters::FirstOrderPoleZero;
+
+/// @brief Frequency parameter to be set in order to have a near-passthrough
+static const float kPassthroughFrequency(0.5f);
+/// @brief Resonance parameter to be set in order to have a near-passthrough
+static const float kPassthroughResonance(0.0f);
+
+/// @brief Filters a random signal, check for mean close to the one
+/// of the input signal (no DC offset introduced)
+TEST(Filters, FirstOrderPoleZeroOutputMean) {
+  for (unsigned int iterations(0); iterations < kIterations; ++iterations) {
+    IGNORE(iterations);
+
+    // Random normalized frequency
+    const float kFrequency(kFreqDistribution(kRandomGenerator));
+    FirstOrderPoleZero filter;
+
+    filter.SetParameters(kFrequency, kPassthroughResonance);
+    Sample expected_mean(Fill(0.0f));
+    Sample actual_mean(Fill(0.0f));
+    for (unsigned int i(0);
+         i < kDataTestSetSize;
+         i += soundtailor::SampleSize) {
+      const Sample input(Fill(kNormDistribution(kRandomGenerator)));
+      const Sample filtered(filter(input));
+      actual_mean = Add(actual_mean, filtered);
+      expected_mean = Add(expected_mean, input);
+    }
+
+    const float kActual(AddHorizontal(actual_mean));
+    const float kExpected(AddHorizontal(expected_mean));
+    const float kEpsilon(1e-3f * kDataTestSetSize);
+
+    EXPECT_NEAR(kExpected, kActual, kEpsilon);
+  }  // iterations?
+}
+
+/// @brief Filters a random signal with max frequency cutoff and default Q
+/// Check for minimal output/input error
+TEST(Filters, FirstOrderPoleZeroPassthrough) {
+  FirstOrderPoleZero filter;
+
+  filter.SetParameters(kPassthroughFrequency, kPassthroughResonance);
+
+  Sample diff_mean(Fill(0.0f));
+  for (unsigned int i(0); i < kDataTestSetSize; i += soundtailor::SampleSize) {
+    const Sample input(Fill(kNormDistribution(kRandomGenerator)));
+    const Sample filtered(filter(input));
+    diff_mean = Add(diff_mean, Sub(filtered, input));
+  }
+  const float kExpected(0.0f);
+  const float kActual(AddHorizontal(diff_mean));
+  const float kEpsilon(1e-6f * kDataTestSetSize);
+
+  EXPECT_NEAR(kExpected, kActual, kEpsilon);
+}
+
+/// @brief Check output range behaviour with max Q parameter without overshoot
+TEST(Filters, FirstOrderPoleZeroRange) {
+  FirstOrderPoleZero filter;
+
+  filter.SetParameters(kPassthroughFrequency, kPassthroughResonance);
+
+  // Very high Epsilon due to this filter implementation
+  const float kEpsilon(1e-1f);
+  for (unsigned int i(0); i < kDataTestSetSize; i += soundtailor::SampleSize) {
+    const Sample input(Fill(kNormDistribution(kRandomGenerator)));
+    const Sample filtered(filter(input));
+    EXPECT_TRUE(GreaterEqual(1.0f, Add(filtered, Fill(-kEpsilon))));
+    EXPECT_TRUE(LessEqual(-1.0f, Add(filtered, Fill(kEpsilon))));
+  }
+}
+
+/// @brief Filters random data (performance test)
+TEST(Filters, FirstOrderPoleZeroPerf) {
+  for (unsigned int iterations(0); iterations < kIterations; ++iterations) {
+    IGNORE(iterations);
+
+    const float kFrequency(kFreqDistribution(kRandomGenerator));
+    FirstOrderPoleZero filter;
+    filter.SetParameters(kFrequency, kPassthroughResonance);
+
+    unsigned int sample_idx(0);
+    while (sample_idx < kFilterDataPerfSetSize) {
+      const Sample kCurrent(Fill(kNormDistribution(kRandomGenerator)));
+      // No actual test!
+      EXPECT_TRUE(LessEqual(-2.0f, filter(kCurrent)));
+      sample_idx += soundtailor::SampleSize;
+    }
+  }
+}
