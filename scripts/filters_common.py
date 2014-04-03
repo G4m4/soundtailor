b98570eefbc3a171c50a36dc9402dd2a623fb872
Gama <misept.dieseneuf@gmail.com>
2014-04-03 11:57:40 +0200

    FILTERS: Add a 1st order Pole-zero low pass filter prototype
    Also added a vectorized version of the processing method.
    Fixes #16.
    
    Signed-off-by: Gama <misept.dieseneuf@gmail.com>

--- scripts/filters_common.py
+++ scripts/filters_common.py
@@ -140,3 +140,102 @@ class FilterInterface(object):
 
     def ProcessSample(self):
         raise Exception("'Pure virtual' function called!")
+class PoleZeroLowPass(FilterInterface):
+    '''
+    Implements a simple 1 pole Low pass
+    '''
+    def __init__(self):
+        self._gain = 0.0
+        self._coeff = 0.0
+        self._last_input = 0.0
+        self._last_output = 0.0
+
+    def SetParameters(self, frequency, resonance):
+        '''
+        Sets frequency (normalized, < 0.5)
+        '''
+        b = math.pi * frequency
+        self._coeff = (2.0 * math.sin(b)) / (math.cos(b) + math.sin(b))
+
+    def ProcessSample(self, sample):
+        '''
+        Actual process function
+        '''
+        direct = sample + self._last_input
+        direct *= self._coeff / 2.0
+        out = direct + self._last_output * (1.0 - self._coeff)
+
+        self._last_input = sample
+        self._last_output = out
+
+        return out
+
+    def Process4Samples(self, vector):
+        '''
+        Actual process function - vectorized version
+        '''
+        direct_v = numpy.add(vector, RotateOnRight(vector, self._last_input))
+        direct_v = MulConst(direct_v, self._coeff / 2.0)
+        oldest_out = direct_v[0] + self._last_output * (1.0 - self._coeff)
+        old_out = direct_v[1] + oldest_out * (1.0 - self._coeff)
+        new_out = direct_v[2] + old_out * (1.0 - self._coeff)
+        newest_out = direct_v[3] + new_out * (1.0 - self._coeff)
+
+        out = (oldest_out, old_out, new_out, newest_out)
+
+        self._last_input = vector[3]
+        self._last_output = out[3]
+
+        return out
+
+if __name__ == "__main__":
+    '''
+    Various tests/sandbox
+    '''
+    import generator_sawtoothdpw
+    import numpy
+    import pylab
+    import utilities
+
+    freq = 110.0
+    sampling_freq = 48000.0
+    length = 1024
+    filter_freq = 2400.0 / sampling_freq
+
+    in_data = utilities.GenerateData(200, 2000, length, sampling_freq)
+    generator = generator_sawtoothdpw.SawtoothDPW(sampling_freq)
+    generator.SetFrequency(freq)
+    for idx, _ in enumerate(in_data):
+        in_data[idx] = generator.ProcessSample()
+
+    out_data = numpy.zeros(length)
+    out_data_vectorized = numpy.zeros(length)
+
+    lowpass = PoleZeroLowPass()
+    lowpass.SetParameters(filter_freq, 0.0)
+
+    for idx, sample in enumerate(in_data):
+        out_data[idx] = lowpass.ProcessSample(sample)
+
+    # Vectorized processing
+    lowpass_v = PoleZeroLowPass()
+    lowpass_v.SetParameters(filter_freq, 0.0)
+    idx = 0
+    while idx < len(in_data):
+        current_vector = (in_data[idx],
+                          in_data[idx + 1],
+                          in_data[idx + 2],
+                          in_data[idx + 3])
+        (out_data_vectorized[idx],
+         out_data_vectorized[idx + 1],
+         out_data_vectorized[idx + 2],
+         out_data_vectorized[idx + 3]) = lowpass_v.Process4Samples(current_vector)
+        idx += 4
+
+    print(utilities.PrintMetadata(utilities.GetMetadata(out_data - out_data_vectorized)))
+
+    pylab.plot(in_data, label="in")
+    pylab.plot(out_data, label="out")
+    pylab.plot(out_data_vectorized, label="out_vectorized")
+    pylab.legend()
+    pylab.show()
