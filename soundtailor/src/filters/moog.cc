81aa26e01c8d8c52277426d1d3273647eb15e468
Gama <misept.dieseneuf@gmail.com>
2014-05-04 17:45:28 +0200

    FILTERS: Add Moog filter C++ implementation
    This is a basic C++ implementation, including basic tests.
    
    Fixes #2 and #1.
    
    Signed-off-by: Gama <misept.dieseneuf@gmail.com>

---
+++ soundtailor/src/filters/moog.cc
@@ -0,0 +1,67 @@
+/// @file moog.cc
+/// @brief Implementation of a Moog filter
+/// @author gm
+/// @copyright gm 2014
+///
+/// This file is part of SoundTailor
+///
+/// SoundTailor is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU General Public License as published by
+/// the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+///
+/// SoundTailor is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+///
+/// You should have received a copy of the GNU General Public License
+/// along with SoundTailor.  If not, see <http://www.gnu.org/licenses/>.
+
+#include "soundtailor/src/maths.h"
+
+#include "soundtailor/src/filters/moog.h"
+
+namespace soundtailor {
+namespace filters {
+
+Moog::Moog()
+    : Filter_Base(),
+      frequency_(0.0f),
+      resonance_(0.0f),
+      last_(0.0f),
+      filters_() {
+  // Nothing to do here for now
+}
+
+Sample Moog::operator()(SampleRead sample) {
+  const float actual_input(sample - resonance_ * last_);
+  // Todo(gm): find a more efficient way to do that
+  float tmp_filtered(actual_input);
+  for(MoogLowPassBlock& filter : filters_) {
+    tmp_filtered = filter(tmp_filtered);
+  }
+  const float out(tmp_filtered);
+  last_ = out;
+
+  return out;
+}
+
+void Moog::SetParameters(const float frequency, const float resonance) {
+  SOUNDTAILOR_ASSERT(frequency > 0.0f);
+  SOUNDTAILOR_ASSERT(frequency <= 1.0f);
+  SOUNDTAILOR_ASSERT(resonance >= 0.0f);
+  SOUNDTAILOR_ASSERT(resonance < 4.0f);
+  const float temp(frequency * (1.0f + 0.03617f * frequency
+                                * (4.0f - resonance) * (4.0f - resonance)));
+  frequency_ = 1.25f * temp * (1.0f - 0.595f * temp + 0.24f * temp * temp);
+  resonance_ = resonance * (1.0f + 0.077f * frequency_
+                                - 0.117f * frequency_ * frequency_
+                                - 0.049f * frequency_ * frequency_ * frequency_);
+  for(MoogLowPassBlock& filter : filters_) {
+    filter.SetParameters(frequency_, resonance_);
+  }
+}
+
+}  // namespace filters
+}  // namespace soundtailor
---
+++ soundtailor/src/filters/moog.h
@@ -0,0 +1,54 @@
+/// @file moog.h
+/// @brief Implementation of a Moog filter
+/// @author gm
+/// @copyright gm 2014
+///
+/// This file is part of SoundTailor
+///
+/// SoundTailor is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU General Public License as published by
+/// the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+///
+/// SoundTailor is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+///
+/// You should have received a copy of the GNU General Public License
+/// along with SoundTailor.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef SOUNDTAILOR_SRC_FILTERS_MOOG_H_
+#define SOUNDTAILOR_SRC_FILTERS_MOOG_H_
+
+#include <array>
+
+#include "soundtailor/src/common.h"
+#include "soundtailor/src/filters/filter_base.h"
+#include "soundtailor/src/filters/moog_lowpassblock.h"
+
+namespace soundtailor {
+namespace filters {
+
+/// @brief Moog low pass filter
+class Moog : public Filter_Base {
+ public:
+  Moog();
+  virtual ~Moog() {
+    // Nothing to do here for now
+  }
+  virtual Sample operator()(SampleRead sample);
+  virtual void SetParameters(const float frequency, const float resonance);
+
+ protected:
+  float frequency_;
+  float resonance_;
+  float last_;
+
+  std::array<MoogLowPassBlock, 4> filters_;
+};
+
+}  // namespace filters
+}  // namespace soundtailor
+
+#endif  // SOUNDTAILOR_SRC_FILTERS_MOOG_H_
