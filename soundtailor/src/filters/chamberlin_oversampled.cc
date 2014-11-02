3aeb92161ca7ee23ca405c8885d31e7a36cf9bd3
G4m4 <misept.dieseneuf@gmail.com>
2014-11-02 01:24:29 +0000

    FILTERS: Add an improved version of the Chamberlin SVF
    Implements #15.
    
    Signed-off-by: G4m4 <misept.dieseneuf@gmail.com>

---
+++ soundtailor/src/filters/chamberlin_oversampled.cc
@@ -0,0 +1,68 @@
+/// @file chamberlingoversampled.cc
+/// @brief Oversampled version of the Chamberlin state variable filter
+/// @author gm
+/// @copyright gm 2014
+///
+/// This file is part of SoundTailor
+///
+/// SoundTailor is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU General Public License as published by
+/// the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+///
+/// SoundTailor is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+///
+/// You should have received a copy of the GNU General Public License
+/// along with SoundTailor.  If not, see <http://www.gnu.org/licenses/>.
+
+// std::sin, std::cos
+#include <cmath>
+// std::min
+#include <algorithm>
+
+#include "soundtailor/src/maths.h"
+
+#include "soundtailor/src/filters/chamberlin_oversampled.h"
+
+namespace soundtailor {
+namespace filters {
+
+ChamberlinOversampled::ChamberlinOversampled()
+    : Chamberlin() {
+  // Nothing to do here for now
+}
+
+Sample ChamberlinOversampled::operator()(SampleRead sample) {
+  Chamberlin::operator()(sample);
+  return Chamberlin::operator()(sample);
+}
+
+void ChamberlinOversampled::SetParameters(const float frequency,
+                               const float resonance) {
+  SOUNDTAILOR_ASSERT(frequency >= Meta().freq_min);
+  SOUNDTAILOR_ASSERT(frequency <= Meta().freq_max);
+  SOUNDTAILOR_ASSERT(resonance >= Meta().res_min);
+  SOUNDTAILOR_ASSERT(resonance <= Meta().res_max);
+  // Stability assertion
+  SOUNDTAILOR_ASSERT(frequency * frequency + 2.0f * resonance * frequency < 4.0f);
+
+  damping_ = std::min(resonance, 2.0f - frequency);
+  frequency_ = frequency * (1.22f - 0.22f * frequency * damping_);
+}
+
+const Filter_Meta& ChamberlinOversampled::Meta(void) {
+  static const Filter_Meta metas(0.0f,
+                                 1.0f,
+                                 1.0f,
+                                 0.0f,
+                                 1.0f,
+                                 1.0f,
+                                 0);
+  return metas;
+}
+
+}  // namespace filters
+}  // namespace soundtailor
---
+++ soundtailor/src/filters/chamberlin_oversampled.h
@@ -0,0 +1,45 @@
+/// @file chamberlingoversampled.h
+/// @brief Oversampled version of the Chamberlin state variable filter
+/// @author gm
+/// @copyright gm 2014
+///
+/// This file is part of SoundTailor
+///
+/// SoundTailor is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU General Public License as published by
+/// the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+///
+/// SoundTailor is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+///
+/// You should have received a copy of the GNU General Public License
+/// along with SoundTailor.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef SOUNDTAILOR_SRC_FILTERS_CHAMBERLIN_OVERSAMPLED_H_
+#define SOUNDTAILOR_SRC_FILTERS_CHAMBERLIN_OVERSAMPLED_H_
+
+#include "soundtailor/src/filters/chamberlin.h"
+
+namespace soundtailor {
+namespace filters {
+
+/// @brief Oversampled Chamberlin state variable low pass filter
+class ChamberlinOversampled : public Chamberlin {
+ public:
+  ChamberlinOversampled();
+  virtual ~ChamberlinOversampled() {
+    // Nothing to do here for now
+  }
+  virtual Sample operator()(SampleRead sample);
+  virtual void SetParameters(const float frequency, const float resonance);
+
+  static const Filter_Meta& Meta(void);
+};
+
+}  // namespace filters
+}  // namespace soundtailor
+
+#endif  // SOUNDTAILOR_SRC_FILTERS_CHAMBERLIN_OVERSAMPLED_H_
